d6a940f0b887114202253deb9c709af34f888899
Kim Nybo Andersen <kna@tzulia.dk>
2020-05-13 13:23:20 +0200

    End of Video 39

---
+++ section-06/weird-operators.py
@@ -0,0 +1,17 @@
+# Power
+print(2**3)
+print(4**8)
+print(49**0.5)
+
+
+# Modulus
+print(10%3)
+print(25%4)
+print(13%2)
+print(16%2)
+
+
+# Int Division
+print(10//3)
+print(400//60)
+print(400%60)
